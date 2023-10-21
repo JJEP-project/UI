2889ff09894fc6883afeeaf44e7e550f59417bd9
mirjalol <mirislomovmirjalol@gmail.com>
2023-10-21 13:08:54 +0100

    config set up

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#1B1B1B',
+                    hover: '#494949',
+                },
+                link: {
+                    DEFAULT: '#0284c7',
+                    hover: '#0ea5e9',
+                    back_hover: '#e0f2fe',
+                },
+            }
+        }
+    }
+}
